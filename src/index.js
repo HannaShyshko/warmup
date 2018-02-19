fb7cf0a125c0abbc1edf2ca7831f851b860c705a
Hanna <hannashishko@gmail.com>
2018-02-19 21:59:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+  const cel = temperature;
+  const celToFar = cel * 9 / 5 + 32;
+  console.log(cel + '\xB0C is ' + celToFar + '\xB0C.');
 };
